2f716d321266e89c34aa3a72606edb7eaf9bc593
Yeichang Choi <ycchoi@nkia.co.kr>
2018-09-27 15:40:42 +0900

    6-2 Promises

--- src/App.js
+++ src/App.js
@@ -4,10 +4,26 @@ import './App.css';
 import Movie from './Movie'
 
 class App extends Component {
+
+  state = {}
+
+  componentDidMount() {
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    })
+  }
+  
   render() {
     return (
       <div className="App">
-        <Movie />
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
